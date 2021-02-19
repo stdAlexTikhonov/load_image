c632d3450a4447a37911658f1f1b4c9e35d3b1e4
stdAlexTikhonov <std1992@mail.ru>
2021-02-19 10:19:46 +0300

    TASK-1 Load Image in canvas

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+const load_btn = document.createElement('input');
+load_btn.type = 'file';
+load_btn.accept = ".png,.jpg,.jpeg";
+document.body.appendChild(load_btn);
+
+const canvas = document.createElement('canvas'), context = canvas.getContext('2d');
+document.body.appendChild(canvas);
+
+
+load_btn.onchange = e => {
+  const file = e.target.files[0];
+  
+  let reader = new FileReader();
+
+  reader.readAsDataURL(file);
+
+  reader.onload = e => {
+    const base_image = new Image();
+    base_image.src = reader.result;
+    base_image.onload = () => {
+      canvas.width = base_image.width;
+      canvas.height = base_image.height;
+      context.drawImage(base_image, 0, 0);
+    }
+
+  }
+}
